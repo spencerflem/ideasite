d63bc91db2fe1b185f3e3e9160e826531cabe4f1
Spencer <spencerflem@gmail.com>
2016-02-22 14:36:54 -0500

    breaking changes
    FIGURE OUT

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,9 +2,6 @@ module CommentsHelper
   def comments_tree_for(comments)
     comments.map do |comment, nested_comments|
       haml_tag :li, :class => comment.look do
-        p comment
-        p 'lol'
-        p comment.look
         haml_concat render(comment)
         if nested_comments.size > 0
           haml_tag :ul do
